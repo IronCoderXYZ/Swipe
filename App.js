82f83b634239b709e93b1f96c66fb42a75885e89
Mitch Hankins <mitchhankins92@gmail.com>
2017-08-18 17:41:40 -0600

    Animated ball component

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Ball from './src/Ball';
 
@@ -16,7 +16,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
